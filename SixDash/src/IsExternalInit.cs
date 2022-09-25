63c2456805547962f70166be0a1ba8e2cebbe8d6
ConfiG <cgytrus@cgyt.ru>
2022-09-25 14:37:57 +0300

    add IsExternalInit for full C# 9 records support

---
+++ SixDash/src/IsExternalInit.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// slightly modified by ConfiG
+// original file: https://github.com/dotnet/runtime/tree/main/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/IsExternalInit.cs
+
+// ReSharper disable All
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// Reserved to be used by the compiler for tracking metadata.
+/// This class should not be used by developers in source code.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class IsExternalInit { }
